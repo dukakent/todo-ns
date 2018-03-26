c44071d77f311a39f3a5848989dc6fb6cc7c6daf
Inokentii Duka <fockewulf3@gmail.com>
2018-03-26 16:02:10 +0300

    chore(todos): remove guard

--- app/todos/todos-routing.module.ts
+++ app/todos/todos-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes } from '@angular/router';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 
 import { TodosPageComponent } from './containers/todos-page/todos-page.component';
-import { LoggedUserGuard } from '../auth/guards/logged-user.guard';
 import { AuthModule } from '../auth/auth.module';
 
 const routes: Routes = [
-  {path: '', component: TodosPageComponent, canActivate: [LoggedUserGuard]}
+  {path: '', component: TodosPageComponent}
+  // {path: '', component: TodosPageComponent, canActivate: [LoggedUserGuard]}
 ];
 
 @NgModule({
